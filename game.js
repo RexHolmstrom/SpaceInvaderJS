fd012e603365c13c96f8d9e8b60ca33fdd95cd1a
rexholmstrom <rexholmstrom96@gmail.com>
2020-08-24 18:28:45 +0200

    update

--- game.js
+++ game.js
@@ -161,9 +161,9 @@ Player.prototype.update = function (dt) {
 // ----------------------------------------------------------------------------
 // Renderer Object'
 
-var renderer = function () {
+var renderer = (function () {
   var _canvas = document.getElementById("game-layer"),
-    _context = _canvas.getContext("2nd"),
+    _context = _canvas.getContext("2d"),
     _enemyColors = [
       "rgb(150, 7, 7)",
       "rgb(150, 89, 7)",
@@ -171,4 +171,37 @@ var renderer = function () {
       "rgb(7, 150, 122)",
       "rgb(46, 7, 150)",
     ];
-};
+
+  function _drawRectangle(color, entity) {
+    _context.fillStyle = color;
+    _context.fillRect(
+      entity.position.x - entity.width / 2,
+      entity.position.y - entity.height / 2,
+      entity.width,
+      entity.height
+    );
+  }
+
+  function _render(dt) {
+    _context.fillStyle = "black";
+    _context.fillRect(0, 0, _canvas.width, _canvas.height);
+
+    var i,
+      entity,
+      entities = game.entities();
+
+    for (i = entities.length - 1; i >= 0; i--) {
+      entity = entities[i];
+
+      if (entity instanceof Enemy) {
+        _drawRectangle(_enemyColors[entity.rank], entity);
+      } else if (entity instanceof Player) {
+        _drawRectangle("rgb(255, 255, 0)", entity);
+      }
+    }
+  }
+
+  return {
+    render: _render,
+  };
+})();
